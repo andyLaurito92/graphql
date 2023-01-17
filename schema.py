ae469ffcc996d747eb896671b907ee1973adf4e4
Andres Laurito Giber <andreslauritogiber@WEST002032.bayer.cnb>
2023-01-17 13:01:17 +0100

    Revert "Creating graphql endpoint for Countries"
    This reverts commit f29c9b282bbe7efc6f00fc47c856d811f2d104a7.

--- schema.py
+++ schema.py
@@ -1,27 +1,23 @@
-#-------------------------------------------------------------------------------
-# Snippet to use python-oracledb in SQLAlchemy 1.4
-# (not needed in SQLAlchemy 2)
+from graphene import ObjectType, String, Schema
 
-import oracledb
-import sys
-oracledb.version = "8.3.0"
-sys.modules["cx_Oracle"] = oracledb
-
-# end snippet
-
-#-------------------------------------------------------------------------------
-from graphene import ObjectType, NonNull, String, List, Schema
-from magic_connection import create_magic_engine
-import pandas as pd
-
-magic_pool_engine = create_magic_engine()
+# Let's define the following schema
+# type Query {
+#   hello(firstName: String = "stranger"): String
+#   goodbye: String
+# }
 
 class Query(ObjectType):
     # this defines a Field `hello` in our Schema with a single Argument `first_name`
     # By default, the argument name will automatically be camel-based into firstName in the generated schema
-    all_countries = List(String)
+    hello = String(first_name=String(default_value="stranger"))
+    goodbye = String()
 
-    def resolve_all_countries(root, info):
-        return [tup[1] for tup in magic_pool_engine.execute("SELECT * FROM MAGIC_POOL.CUCNOB_COUNTRY_CNT").fetchall()]
+    # our Resolver method takes the GraphQL context (root, info) as well as
+    # Argument (first_name) for the Field and returns data for the query Response
+    def resolve_hello(root, info, first_name):
+        return f'Hello {first_name}!'
+
+    def resolve_goodbye(root, info):
+        return 'See ya!'
 
 schema = Schema(query=Query)
